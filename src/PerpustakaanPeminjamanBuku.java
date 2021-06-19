225af98f283d0c8eb50480927e389678f443bf76
RINA <81545047+rinaapriani@users.noreply.github.com>
2021-06-19 20:27:36 +0800

    Subclass02 3 Atribut 2 Method

---
+++ src/PerpustakaanPeminjamanBuku.java
@@ -0,0 +1,20 @@
+public class PerpustakaanPeminjamanBuku {
+    //atribut
+    public String NamaPeminjam;
+    public String JudulBukuDipinjam;
+    public int JumlahBukuDipinjam;
+
+    //method
+    public void TampilkanInfoDataPeminjam(){
+        System.out.println("=====================");
+        System.out.println("Nama Peminjam : " +NamaPeminjam);
+        System.out.println("Judul Buku : " +JudulBukuDipinjam);
+        System.out.println("Jumlah Buku Dipinjam : " +JumlahBukuDipinjam);
+    }
+
+    public boolean create(){
+        String insertSQL = "INSERT INTO perpustakaanpeminjamanbuku VALUES ('"+NamaPeminjam+"','"+JudulBukuDipinjam+"','"+JumlahBukuDipinjam+"')";
+        System.out.println(insertSQL);
+        return false;
+    }
+}
